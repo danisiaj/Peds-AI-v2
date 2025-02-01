71580462894c82f3b438abcd064596fbc50d2a0f
danisiaj <123707285+danisiaj@users.noreply.github.com>
2025-02-01 04:10:36 +0100

    Update edu.py

--- app/pages/admin/edu.py
+++ app/pages/admin/edu.py
@@ -25,7 +25,7 @@ def load_data():
     It also builds a dataframe widget in the app for visualization purposes
     """
 
-    nurses_data = pd.read_csv('./data/nurses_dataset.csv')
+    nurses_data = pd.read_csv('data/nurses_dataset.csv')
 
     return nurses_data
 
