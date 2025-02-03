38fb5288338fccf67b3249dc6af91d943a950d03
danisiaj <123707285+danisiaj@users.noreply.github.com>
2025-02-03 17:47:40 +0100

    Update chatbot.py

--- app/pages/patient/chatbot.py
+++ app/pages/patient/chatbot.py
@@ -96,10 +96,10 @@ def set_up_page():
     st.header('Peds Cardiology Chatbot')
     with st.expander('Explanation'):
         st.info("""Langchain RAG Model AI Assistant:
-            \n a. Data: Pediatric Cardiology The Essential Pocket Guide. Rights reserved*\n
-            \n b. Embeddings: Open AI Embeddings
-            \n c. Database: Qdrant Cloud Vector Store
-            \n d. LLM: Open AI\n
+            \n      a. Data: Pediatric Cardiology The Essential Pocket Guide. Rights reserved*\n
+            \n      b. Embeddings: Open AI Embeddings
+            \n      c. Database: Qdrant Cloud Vector Store
+            \n      d. LLM: Open AI\n
             \n\n AI CHATBOT to answer questions about Pediatric Cardiology. 
                 User's queries will be store using MySQL for further analysis.
             """, 
