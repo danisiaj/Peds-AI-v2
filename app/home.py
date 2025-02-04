1b2c5769de7dce0d399e92fd9de5a683354d2a18
danisiaj <123707285+danisiaj@users.noreply.github.com>
2025-02-04 19:07:26 +0100

    Update home.py

--- app/home.py
+++ app/home.py
@@ -49,10 +49,10 @@ def login():
     with st.expander(label='Log in instructions'):
                 
         st.info("""
-                Login info:\n 
-    username/last name = user\n 
+                \nLogin info:\n 
+    username/last name = user 
     password/MRN = 12345\n
-                This APP integrates almost all the concepts learned in my Data Science & Machine Learning Bootcamp through IronHack.
+            \nThis APP integrates almost all the concepts learned in my Data Science & Machine Learning Bootcamp through IronHack.
             \n Peds AI App was created by a nurse who works in a Pediatric Cardiology Floor in the hospital. The goal of the app is to enhance education for patients, family members and providers. 
             \n Peds AI adds different features depending WHO is logging in, giving special priviledges to Educators and Administrators.
 
